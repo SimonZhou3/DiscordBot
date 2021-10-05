baaddaf0f129395ac69b35c57402ebcc157c3825
Simon Zhou <Simonzhou3@hotmail.com>
2021-10-04 20:27:18 -0700

    fixed misc

--- target/DiscordBot-1.0-SNAPSHOT/bot/Main.java
+++ target/DiscordBot-1.0-SNAPSHOT/bot/Main.java
@@ -7,10 +7,13 @@ import net.dv8tion.jda.api.entities.Activity;
 
 public class Main {
 
+
+
     public static JDABuilder jda;
 
 
     public static void main (String args[]) throws Exception {
+
            // Activates discord Bot using secure dotenv to keep API key safe
             jda = JDABuilder.createDefault(System.getenv("MY_ENV_APIKEY"));
             jda.setActivity(Activity.streaming(" -help","https://www.twitch.tv/gumsf"));
