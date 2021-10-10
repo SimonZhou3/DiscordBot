5af706dec84eeac97ff410f7a6b3b4de6e66869b
Simon Zhou <Simonzhou3@hotmail.com>
2021-10-09 21:42:44 -0700

    Applied new Key for more security purposes

--- target/DiscordBot-1.0-SNAPSHOT/bot/Main.java
+++ target/DiscordBot-1.0-SNAPSHOT/bot/Main.java
@@ -7,17 +7,12 @@ import net.dv8tion.jda.api.entities.Activity;
 
 public class Main {
 
-
-
     public static JDABuilder jda;
 
 
     public static void main (String args[]) throws Exception {
-
            // Activates discord Bot using secure dotenv to keep API key safe
-//            Dotenv dotenv = Dotenv.load();
             jda = JDABuilder.createDefault(System.getenv("MY_ENV_APIKEY"));
-//            jda = JDABuilder.createDefault(dotenv.get("MY_ENV_APIKEY"));
             jda.setActivity(Activity.streaming(" -help","https://www.twitch.tv/gumsf"));
             JDA bot;
             bot = jda.build();
